ce42f402aaee32915f3a0ea1511734b293cf6bdf
narshaKim <narsha9501@gmail.com>
2019-07-21 13:18:13 +0900

    2.4 스프링 테스트 적용 2.4.1 테스트를 위한 애플리케이션 컨텍스트 관리

--- solip-spring/src/test/java/UserDaoTest.java
+++ solip-spring/src/test/java/UserDaoTest.java
@@ -4,24 +4,34 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/spring-config.xml")
 public class UserDaoTest {
 
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
     private UserDao userDao;
+
     private User user1;
     private User user2;
     private User user3;
 
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("spring-config.xml");
 
-        userDao = context.getBean("userDao", UserDao.class);
+        System.out.println(this);
+        System.out.println(this.context);
 
         user1 = new User("aaa", "AAA", "aPassword");
         user2 = new User("bbb", "BBB", "bPassword");
