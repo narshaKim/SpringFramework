fd048751eb1cf3b829c237ce73f61315d904937a
narshaKim <narsha9501@gmail.com>
2019-07-28 17:47:35 +0900

    4 예외 4.1 사라진 SQLException

--- solip-spring/src/main/java/dao/UserDao.java
+++ solip-spring/src/main/java/dao/UserDao.java
@@ -1,15 +1,10 @@
 package dao;
 
 import domain.User;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -29,24 +24,24 @@ public class UserDao {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(final User user) throws SQLException {
+    public void add(final User user) {
         jdbcTemplate.update("INSERT INTO TB_USER(ID, NAME, PASSWORD) VALUES(?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
-    public User get(final String id) throws SQLException {
+    public User get(final String id) {
         return jdbcTemplate.queryForObject("SELECT * FROM TB_USER WHERE id = ?", new Object[]{id}, this.userMapper);
     }
 
-    public List<User> getAll() throws SQLException {
+    public List<User> getAll() {
         List<User> users = jdbcTemplate.query("SELECT * FROM TB_USER", this.userMapper);
         return users;
     }
 
-    public void deleteAll() throws SQLException {
+    public void deleteAll() {
         jdbcTemplate.update("DELETE FROM TB_USER");
     }
 
-    public long getCount() throws SQLException {
+    public long getCount() {
         Integer result = jdbcTemplate.queryForObject("SELECT COUNT(*) AS COUNT FROM TB_USER", Integer.class);
         return result;
     }
