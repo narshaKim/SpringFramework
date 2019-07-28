e859e212e4875e8fba8e4f5e274a127f6fd87cc4
narshaKim <narsha9501@gmail.com>
2019-07-28 20:25:10 +0900

    5 서비스 추상화 5.1 사용자 레벨 관리 기능 추가 5.1.3 UserService.upgradeLevels()

---
+++ solip-spring/src/main/java/service/UserService.java
@@ -0,0 +1,37 @@
+package service;
+
+import dao.UserDao;
+import domain.Level;
+import domain.User;
+
+import java.util.List;
+
+public class UserService {
+
+    UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+
+        for(User user : users) {
+            boolean changed = false;
+
+            if(user.getLevel()==Level.BASIC && user.getLogin()>=50) {
+                user.setLevel(Level.SILVER);
+                changed=true;
+            }
+            else if(user.getLevel()==Level.SILVER && user.getRecommend()>=30) {
+                user.setLevel(Level.GOLD);
+                changed=true;
+            }
+
+            if(changed)
+                userDao.update(user);
+        }
+    }
+
+}
---
+++ solip-spring/src/test/java/UserServiceTest.java
@@ -0,0 +1,69 @@
+import dao.UserDao;
+import domain.Level;
+import domain.User;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import service.UserService;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/spring-config.xml")
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserDao userDao;
+
+    List<User> users;
+
+    @Test
+    public void bean() {
+        Assert.assertThat(userService, CoreMatchers.is(CoreMatchers.notNullValue()));
+    }
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0)
+                , new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0)
+                , new User("erwins", "신승한", "p3", Level.SILVER, 60, 29)
+                , new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30)
+                , new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    public void upgradeLevels() {
+
+        userDao.deleteAll();
+
+        for(User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        Assert.assertThat(userUpdate.getLevel(), CoreMatchers.is(expectedLevel));
+    }
+
+}
